f5ef5a3d28fc60449ad3f2c96a652174ea20cc75
Gaba <gaba@datauy.org>
2013-10-26 12:25:23 +0100

    Skeleton for api

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ get '/' do
   "API to get gender on a name."
 end
 
-get '/gender' do 
-  'hello world'
+
+# /name GET or POST a full name -- return a gender probability
+get '/name' do 
 end
+
+# GET or POST fulltext -- return pronoun gender
+get '/content' do
+end
+
+# GET OR POST fulltext -- return list of tokens and their likely gender
